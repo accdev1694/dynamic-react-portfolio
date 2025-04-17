5af029cc6a60c9a512d437776aaf9403c62de05a
unknown <diloch111@gmail.com>
2025-04-17 23:29:40 +0100

    complete navbar styling, navigation logic, hamburger menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,30 +1,38 @@
 import { getImgUrl } from "../../utils";
 import styles from "./Navbar.module.css";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <div className={styles.navBar}>
-      <a className={styles.title} href="/">Portfoli<span className={styles.emphasisedText}>o</span></a>
+      <a className={styles.title} href="/">
+        Portfoli<span className={styles.emphasisedText}>o</span>
+      </a>
       <div className={styles.menu}>
         <img
+          onClick={() => setMenuOpen(!menuOpen)}
           className={styles.menuBtn}
-          src={getImgUrl("/assets/nav/menuIcon.png")}
+          src={menuOpen ? getImgUrl("/assets/nav/closeIcon.png") : getImgUrl("/assets/nav/menuIcon.png")}
           alt="Menu Button"
         />
-        <ul className={styles.menuList}>
-          <li>
-            <a href="/">About</a>
-          </li>
-          <li>
-            <a href="#skills">Skills</a>
-          </li>
-          <li>
-            <a href="#Projects">Projects</a>
-          </li>
-          <li>
-            <a href="#contact">Contact</a>
-          </li>
-        </ul>
+        <div className={styles.menuListContainer}>
+          <ul className={`${styles.menuList} ${menuOpen && styles.menuList + " " + styles.active}`} onClick={()=> setMenuOpen(false)}>
+            <li>
+              <a href="/">About</a>
+            </li>
+            <li>
+              <a href="#skills">Skills</a>
+            </li>
+            <li>
+              <a href="#Projects">Projects</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
